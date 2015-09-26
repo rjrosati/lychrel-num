bd461aee966c16ee64872b685e305e663588409d
Robbie Rosati <rjrosati@utexas.edu>
2015-09-26 08:49:54 +0000

    in process of rolling own big integer code, with intelligent reverse addition.

--- findchain.cpp
+++ findchain.cpp
@@ -1,13 +1,59 @@
-#include <gmpxx.h>
+//#include <gmpxx.h>
 #include <stdio.h>
 #include <iostream>
-#include <algorithm>
+#include <sstream>
 #include <omp.h>
-
+#include <vector>
 
 #define STEP_LIMIT 270 
 
+typedef unsigned char radix_t; // should be smaller than unsigned int
+
+class bigint {
+  private:
+    std::vector<radix_t> digits;
+    radix_t radix;
+  public:
+    bigint(unsigned long long,radix_t);
+    bigint& operator+=(bigint&);
+    bigint& operator^(bigint&);
+    operator std::string() const {
+        std::stringstream base10digits;
+        for (int i=digits.size();i>0;i--) {
+            base10digits << digits[i];
+        }
+        return base10digits.str();
+    }
+};
+
+bigint::bigint(unsigned long long i, radix_t r) : radix(r) { // I'll not hardcode radix to 10 for now...
+    unsigned long long num = i;
+    while (num>0) {
+        this->digits.push_back(num%radix);
+        num /= radix;
+    }
+    // these will end up LSD first, MSD last
+}
+
+bigint& bigint::operator+=(bigint& other) { // normal addition
+    unsigned carry = 0,dig;
+    for (size_t i=0; i<digits.size();i++) {
+        dig = digits[i]+other.digits[i]+carry;
+        digits[i] = dig%radix;
+        carry = dig/radix;
+    }
+    return *this;
+}
+
+
 int main(int argc, char* argv[]) {
+    
+    bigint a(100,10);
+    bigint b(300,10);
+
+    std::cout<<std::string(a+=b)<<std::endl;
+    
+    /*
     mpz_class x = 0, y, steps = 0,init;
     
     init.set_str(argv[1],10);
@@ -30,6 +76,6 @@ int main(int argc, char* argv[]) {
         }
         if (steps<STEP_LIMIT+1 && steps>100) std::cout<<i+init<<","<<steps<<std::endl;
     }
-
+    */
     return 0;
 }
