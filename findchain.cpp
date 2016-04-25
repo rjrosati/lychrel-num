887bb26ff3c63ea18524b0ece375f12656d8cfa4
Robbie Rosati <rjrosati@utexas.edu>
2016-04-25 03:15:47 -0500

    modified code for benchmarking. On my desktop gets about 1.4M reverse-adds/sec/core

--- findchain.cpp
+++ findchain.cpp
@@ -8,8 +8,8 @@
 
 #include "bigint.h"
 
-#define STEP_LIMIT 390 
-#define PRINT_LIMIT 100
+#define STEP_LIMIT 290 
+#define PRINT_LIMIT 150
 
 #define NUMS_PER_NODE (int)1e9
 
@@ -21,9 +21,8 @@ int main(int argc, char* argv[]) {
 
     if (myrank==0) {
         if (argc != 2) {
-            std::cout<<"Usage:\n\t./findchain [start of search as unsigned long long]\n";
+            std::cout<<"Usage:"<<std::endl<<"\t./findchain [start of search as unsigned long long]"<<std::endl;
             MPI_Abort(MPI_COMM_WORLD,-1);
-            return 0;
         }
     }
 
@@ -31,17 +30,22 @@ int main(int argc, char* argv[]) {
 
     myinit = myinit + myrank*NUMS_PER_NODE; // so we'll cover mysize*NUMS_PER_NODE numbers in total
     if (myrank == 0) {
-        std::cout<<"checking for palindromic sequences shorter than "<<STEP_LIMIT<<" steps\n";
-        std::cout<<"will print if more than "<<PRINT_LIMIT<<" steps\n";
-        std::cout<<"will search "<<std::string(myinit)<<" --> "<<std::string(myinit+mysize*NUMS_PER_NODE)<<"\n";
-        std::cout<<mysize<<" nodes\n";
-        std::cout<<omp_get_num_procs()<<" threads each\n";
+        std::cout<<"checking for palindromic sequences shorter than "<<STEP_LIMIT<<" steps"<<std::endl;
+        std::cout<<"will print if more than "<<PRINT_LIMIT<<" steps"<<std::endl;
+        std::cout<<"will search "<<std::string(myinit)<<" --> "<<std::string(myinit+mysize*NUMS_PER_NODE)<<std::endl;
+        std::cout<<mysize<<" nodes"<<std::endl;
+        std::cout<<omp_get_num_procs()<<" threads each"<<std::endl;
     }
-    int steps;
+    int steps=0,steptimer=0;
+    double tic=0,toc=0;
 
-    omp_set_num_threads(omp_get_num_procs());
-    #pragma omp parallel for shared(myinit) private(steps) schedule(dynamic,1000)
+    omp_set_num_threads(1);//omp_get_num_procs());
+    #pragma omp parallel for shared(myinit) firstprivate(tic,toc,steps,steptimer) schedule(dynamic,NUMS_PER_NODE/1000)
     for (int i=0; i<NUMS_PER_NODE; i++) {
+        if (i%100000 == 0) {
+            tic = omp_get_wtime();
+            steptimer = 0;
+        }
         bigint x = myinit+i;
         steps = 0;
         while (true) {
@@ -51,6 +55,12 @@ int main(int argc, char* argv[]) {
             steps++;
         }
         if (steps<STEP_LIMIT+1 && steps > PRINT_LIMIT) std::cout<<std::string(myinit+i)<<","<<steps<<","<<std::string(x)<<std::endl;
+        steptimer+=steps;
+        if (i%100000 == 99999) {
+            toc = omp_get_wtime();
+            std::cout<<steptimer/(toc-tic)<<" radds/sec"<<std::endl;
+            steptimer = 0;
+        }
     }
 
     MPI_Finalize();
